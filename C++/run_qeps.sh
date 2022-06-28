d8c42d777724aacc7891b6e80c75f1fb273572dd
axhertz <axel.hertzschuch@tu-dresden.de>
2022-06-28 11:17:47 +0200

    rm sel-aware qep-s

--- C++/run_qeps.sh
+++ C++/run_qeps.sh
@@ -8,22 +8,10 @@ rm cmake_install.cmake
 rm Makefile
 cd ..
 
-cd analyseSelAwareQEPS
-rm -rf CMakeFiles
-rm CMakeCache.txt
-rm cmake_install.cmake
-rm Makefile
-cd ..
-
 
 cmake build  -B./analysePlainAndFilter -S./analysePlainAndFilter/ -DCMAKE_BUILD_TYPE=Release
 cmake --build ./analysePlainAndFilter/
 
-cmake build  -B./analyseSelAwareQEPS -S./analyseSelAwareQEPS/ -DCMAKE_BUILD_TYPE=Release
-cmake --build ./analyseSelAwareQEPS/
-
 
 ./analysePlainAndFilter/analyseQEPSplain
-
-./analyseSelAwareQEPS/analyseQEPS
 
